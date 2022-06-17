8bfdc404cfe0cffd48ac10d5025e2aeda3bfd219
Jose <ls.joseignacio@gmail.com>
2022-06-17 14:17:32 +0200

    Cleans up code

--- res-mod/res-mod.py
+++ res-mod/res-mod.py
@@ -1,6 +1,9 @@
 from os import listdir
+import PIL
 from PIL import Image
 from math import floor
+
+PIL.Image.MAX_IMAGE_PIXELS = 933120000
 
 SUPPORTED_TYPES = ["jpg","png","jpeg"]
 
@@ -11,14 +14,19 @@ print("Supported formats: "+", ".join(SUPPORTED_TYPES))
 newWidth = int(input("Output width: "))
 
 imageFiles = [file for file in listdir(".") if file.split(".")[-1] in SUPPORTED_TYPES and TAG not in file]
-
 for file in imageFiles:
-    name, format = file.split(".")
-    im = Image.open(file)
-    size = im.size
+    try:
+        print(f'processing file {file}')
+        name, format = file.split(".")
+        im = Image.open(file)
+        size = im.size
 
-    modRes = (newWidth, int(floor(size[1] * newWidth / size[0])))
+        modRes = (newWidth, int(floor(size[1] * newWidth / size[0])))
 
-    resizedIm = im.resize(modRes)
-    resizedIm.save(name+TAG+"."+format)
+        resizedIm = im.resize(modRes)
+        resizedIm.save(name+TAG+"."+format)
+        print(f'image resized')
+    except:
+        print(f'error processing the image')
 
+input()
