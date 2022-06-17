3a18294c4efe8589a2e731ced3a2e78cbe87a9e8
Jose <ls.joseignacio@gmail.com>
2022-06-17 12:45:53 +0200

    Fixes errors obtaining file list and calculating output resolution

--- res-mod/res-mod.py
+++ res-mod/res-mod.py
@@ -4,18 +4,20 @@ from math import floor
 
 SUPPORTED_TYPES = ["jpg","png","jpeg"]
 
-TAG = "_tag"
+TAG = "_resized"
+
+print("Supported formats: "+", ".join(SUPPORTED_TYPES))
 
 newWidth = int(input("Output width: "))
 
-imageFiles = [file for file in listdir() if file.split(".")[-1] in SUPPORTED_TYPES and TAG not in file]
+imageFiles = [file for file in listdir(".") if file.split(".")[-1] in SUPPORTED_TYPES and TAG not in file]
 
 for file in imageFiles:
     name, format = file.split(".")
     im = Image.open(file)
     size = im.size
 
-    modRes = (newWidth, floor(size[1] * newWidth / size[0]))
+    modRes = (newWidth, int(floor(size[1] * newWidth / size[0])))
 
     resizedIm = im.resize(modRes)
     resizedIm.save(name+TAG+"."+format)
