1661c2999e91d10f8c8c0a7cbf145e0f6292e24e
Jose <ls.joseignacio@gmail.com>
2024-02-17 14:09:22 +0100

    refactor: simplify get content function

--- pngtoico/README.md
+++ pngtoico/README.md
@@ -1,6 +1,6 @@
 # PNGTOICO
 
-**v.0.1.2**
+**v.0.2.0**
 
 ## Descripción
 
--- pngtoico/utils.py
+++ pngtoico/utils.py
@@ -17,11 +17,13 @@ def file_opener(func):
 
 
 def get_content_section(func):
+    def get_content_lines(content):
+        return content.strip().split("\n")[1:]
+
     def wrap(*args, **kwargs):
         match = re.search(f"#{kwargs['section']}[^#]+", kwargs['content'])
-        if not match:
-            return func(*args, [])
-        return func(*args, match.group().strip().split("\n")[1:])
+        return func(*args, get_content_lines(match.group()) if match else [])
+
     return wrap
 
 
